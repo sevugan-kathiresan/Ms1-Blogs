6b9a2fde1059d3d3264da100edbf34591352c3f6
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-17 08:00:19 +1100

    API logic for post service created and tested using postman

---
+++ Blogs/posts/index.js
@@ -0,0 +1,38 @@
+// Imports
+const express = require("express");
+const bodyParser = require('body-parser'); // helps with parsing the request and response
+const { randomBytes } = require("crypto");
+
+// Creating an app instance
+const app = express();
+app.use(bodyParser.json())
+
+// we are not using database for this app so we are creating a JSON object that can hold the posts
+const posts = {};
+
+// Creating our routes
+
+// Route to get all the posts
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+// Route to post a new post
+app.post('/post', (req, res) => {
+    // Generate a random id for the post
+    const id = randomBytes(4).toString('hex'); // 4 - given as argument to create 4 random bytes
+    // retrieve the information from the body
+    const { title } = req.body;
+    // Store the data in our post object as we don't have DB
+    posts[id] = {
+        id, title
+    }
+    // Sending the response
+    res.status(201).send(posts[id]) // 201 - new resource created
+});
+
+app.listen(4000, () => {
+    console.log("Listening on 4000")
+});
+
+
