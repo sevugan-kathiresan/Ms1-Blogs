c82c5e494e4fd5b3e4ef74ed12e7d5d914aa5c79
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-17 13:05:16 +1100

    PostCreate form component rendered on the client side

---
+++ Blogs/client/src/PostCreate.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const PostCreate = ()=>{
+    const [title, setTitle] = useState('');
+
+    // Function to handle submit
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('http://localhost:4000/post', {
+            title
+        });
+        
+        setTitle("");
+    };
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <br/>
+                    <input value={ title } onChange={ e => setTitle(e.target.value)} className="form-control"/>
+                </div>
+                <br/>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default PostCreate
