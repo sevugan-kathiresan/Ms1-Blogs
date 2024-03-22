0738d023bc6a44a710e7b20dafc15055865c557e
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-23 09:25:44 +1100

    Query Service added

--- Blogs/query/index.js
+++ Blogs/query/index.js
@@ -6,13 +6,54 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// InMemory DataStructure to hold the posts and comments
+const posts = {}
+
+// The data structure will look like this
+// posts === {
+//     "j123k42" : {
+//         id : "j123k42",
+//         title: "post title",
+//         comments: [
+//             { id: "k1234", content: "comments"},
+//             { id: "j1234", content: "comments"}
+//         ]
+//     },
+//     "j123k42" : {
+//         id : "j123k42",
+//         title: "post title",
+//         comments: [
+//             { id: "k1234", content: "comments"},
+//             { id: "j1234", content: "comments"}
+//         ]
+//     }
+// }
+
 // Route to send all the post and comments related information to the client
 app.get("/events", (req, res) => {
-
+    
+    res.send(posts);
 });
 
 // Route to receive the events emitted from the event bus
 app.post("/events", (req, res) => {
+    const { type, data } = req.body
+
+    if (type === "postCreated") {
+        const { id, title} = data;
+
+        posts[id] = {id, title, comments: []};
+    }
+
+    if (type === "commentCreated"){
+        const { id, content, postId } = data;
+
+        posts[postId].comments.push({ id, content });
+    }
+
+    //Print out the current state of out In memory posts data structure
+    console.log(posts)
+    res.send({})
 
 });
 
