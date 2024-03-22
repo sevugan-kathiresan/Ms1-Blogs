27607e50bb394fd9972945fc56b0c72a5f6903c3
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-22 19:37:50 +1100

    event emitting functionality added for comments service

--- Blogs/comments/index.js
+++ Blogs/comments/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
 const cors = require("cors");
+const axios = require("axios")
 
 // configuration and instantiation
 const app = express();
@@ -15,7 +16,7 @@ commentsByPostId = {}
 // Routes
 
 // Route to get all the comments associate with a post
-app.get("/post/:id/comments", (req, res) => {
+app.get("/post/:id/comments", async (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 })
 
@@ -28,6 +29,16 @@ app.post("/post/:id/comment", (req, res) => {
     const comments = commentsByPostId[req.params.id] || []; // if undefined give me an empty array && req.params.id - lets us extract the URL params
     comments.push({ id: commentId, content}) // Appending the data to the extracted array
     commentsByPostId[req.params.id] = comments // Assigning back to our in memory data structure
+
+    // emit the event
+    await axios.post("http://localhost:4005/events", {
+        type: commentCreated,
+        data : {
+            id : commentId,
+            content,
+            postId: req.params.id
+        }
+    });
 
     res.status(201).send(comments)
 })
