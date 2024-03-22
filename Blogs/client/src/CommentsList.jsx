9acd75360bfdea8f194e0f0e117f685f4e2af148
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-23 09:48:21 +1100

    Client app modified to make a single get request to our query service endpoint to fetch all the data

--- Blogs/client/src/CommentsList.jsx
+++ Blogs/client/src/CommentsList.jsx
@@ -1,17 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 
-const CommentsList = ({ postId }) => {
-    const [comments, setComments] = useState([])
-
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/post/${postId}/comments`);
-        setComments(res.data);
-    }
-
-    useEffect(()=>{
-        fetchData();
-    },[]);
+const CommentsList = ({ comments }) => {
 
     const renderedComments = comments.map(comment =>{
         return(<li key={comment.id}>{comment.content}</li>);
--- Blogs/client/src/PostsList.jsx
+++ Blogs/client/src/PostsList.jsx
@@ -10,7 +10,7 @@ const PostsList = () => {
 
     // Function to fetch posts from out backend
     const fetchPosts = async () => {
-        const res = await axios.get("http://localhost:4000/posts");
+        const res = await axios.get("http://localhost:4002/posts");
         setPosts(res.data);
     };
 
@@ -23,7 +23,7 @@ const PostsList = () => {
             <div className="card" style={{ width: '30%', marginBottom:'20px'}} key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
-                    <CommentsList postId={post.id}/>
+                    <CommentsList comments={post.comments}/>
                     <CommentCreate postId={post.id}/>
                 </div>
             </div>
