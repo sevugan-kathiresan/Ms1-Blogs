64c0041c3f1631b8a1d392da0ca33e41f0f772ce
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-03-24 11:53:17 +1100

    React client has been modified to work in reponse with the comment moderation

--- Blogs/client/src/CommentsList.jsx
+++ Blogs/client/src/CommentsList.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const CommentsList = ({ comments }) => {
 
     const renderedComments = comments.map(comment =>{
-        return(<li key={comment.id}>{comment.content}</li>);
+        let content;
+
+        if (comment.status === "approved"){
+            content = comment.content;
+        }
+        if (comment.status === "pending"){
+            content = "This content is awaiting moderation";
+        }
+        if (comment.status === "rejected"){
+            content = "This comment has been removed";
+        }
+        return(<li key={comment.id}>{content}</li>);
     });
 
     return(
